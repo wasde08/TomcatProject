c1ee94c2c37e5ce3d733460980f18713ef1e494f
Almaz <wasde08>
2020-04-21 21:24:31 +0300

    init

---
+++ src/main/java/servlet/Listener.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class Listener implements HttpSessionListener {
+
+    private static int totalActiveSessions;
+
+    public static int getTotalActiveSession(){
+        return totalActiveSessions;
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent arg0) {
+        totalActiveSessions++;
+        System.out.println("sessionCreated - add one session into counter");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent arg0) {
+        totalActiveSessions--;
+        System.out.println("sessionDestroyed - deduct one session from counter");
+    }
+
+}
